be0a706340f2a1cbf5d4a8637d5487b2fd53f476
Андрей <info@bustep.ru>
2024-03-15 08:39:23 +0700

    конфигруация

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -11,31 +11,9 @@ import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
 version = "2023.11"
 
 project {
-    buildType(deploy)
-    buildType(building)
     val projectName: String = DslContext.projectId.value.toLowerCase().substringAfter('_')
     params {
         param("env.MINIO_PREFIX", "$projectName.plan")
         param("env.SWARM_PREFIX", "$projectName")
     }
 }
-
-object deploy : BuildType({
-    templates(AbsoluteId("deploy"))
-    name = "deploy"
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-})
-
-object building : BuildType({
-    templates(AbsoluteId("building"))
-    name = "Building"
-
-    val projectName: String = DslContext.projectId.value.toLowerCase().substringAfter('_')
-    vcs {
-        root(DslContext.settingsRoot)
-        checkoutDir = "/var/www/www-data/data/www/discovery/$projectName/%teamcity.build.branch%-$projectName"
-    }
-
-})
