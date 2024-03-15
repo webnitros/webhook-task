b737fe6403fa7ca13847c9067df4638c3d3ddb88
Андрей <info@bustep.ru>
2024-03-15 13:04:19 +0700

    settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,4 +12,42 @@ version = "2023.11"
 
 project {
     val projectName: String = DslContext.projectId.value.toLowerCase().substringAfter('_')
+
+    buildType(Run)
+
+    params {
+        text("env.TASK_ID", "", display = ParameterDisplay.PROMPT, allowEmpty = true)
+        password("env.TASK_API_KEY", "")
+        password("env.TASK_WEBHOOK", "")
+        text("env.TASK_COMMENT", "", display = ParameterDisplay.PROMPT, allowEmpty = true)
+        text("env.TASK_USEREMAIL", "", display = ParameterDisplay.PROMPT, allowEmpty = true)
+        text("env.TASK_SITE_URL", "", display = ParameterDisplay.PROMPT, allowEmpty = true)
+    }
 }
+
+
+object Run : BuildType({
+   name = "Run"
+   vcs {
+       root(DslContext.settingsRoot)
+   }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = """
+                #!/bin/bash
+
+                # Определение переменных
+                API_KEY="%env.TASK_API_KEY%"
+                WEBHOOK="%env.TASK_WEBHOOK%"
+                USEREMAIL="%env.TASK_USEREMAIL%"
+                TASKID="%env.TASK_ID%"
+                SITE_URL="%env.TASK_SITE_URL%"
+                COMMENT="%env.TASK_COMMENT%"
+
+                # Передача аргументов в PHP-файл
+                php Chat/Comment/create.php ${'$'}API_KEY ${'$'}WEBHOOK ${'$'}USEREMAIL ${'$'}TASKID ${'$'}SITE_URL ${'$'}COMMENT
+            """.trimIndent()
+        }
+    }
