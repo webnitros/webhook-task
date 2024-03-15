70fc300ffec6136db4980e40feaf2af2c3e9634e
Андрей <info@bustep.ru>
2024-03-15 13:08:06 +0700

    settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,17 +1,11 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
-import jetbrains.buildServer.configs.kotlin.triggers.retryBuild
-import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
-import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
 
 version = "2023.11"
 
 project {
-    val projectName: String = DslContext.projectId.value.toLowerCase().substringAfter('_')
+    vcsRoot(DslContext.settingsRoot)
 
     buildType(Run)
 
